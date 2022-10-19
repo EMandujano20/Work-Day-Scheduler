11fcdc25f36ee27a64740dc67aa9643549b1ee55
Eduardo Mandujano <mandujanolalo@gmail.com>
2022-10-19 15:07:48 -0700

    added current day and time, Added time rows,

---
+++ Develop/script.js
@@ -0,0 +1,13 @@
+var displayCurrentDay = document.querySelector("currentDay")
+var currentDay = moment();
+displayCurrentDay.textcContext = currentDay.format("dddd, MM, YYYY")
+
+
+//timeblocks
+var timeblocks = $('.time-block').addClass('row');
+var blockText = $("<p>").addClass("description");
+timeBlock.append(blockText);
+
+// 
+var currentDay = parseInt(moment().format('H'));
+
