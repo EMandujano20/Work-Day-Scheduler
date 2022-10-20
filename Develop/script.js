ffe1d89e4911bfab16be3c0ee849bea19a7a2a59
Eduardo Mandujano <mandujanolalo@gmail.com>
2022-10-19 19:30:13 -0700

    cols length

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,3 @@
-var displayCurrentDay = document.querySelector("currentDay")
-var currentDay = moment();
-displayCurrentDay.textcContext = currentDay.format("dddd, MM, YYYY")
-
-
 //timeblocks
 var timeblocks = $('.time-block').addClass('row');
 var blockText = $("<p>").addClass("description");
@@ -11,3 +6,45 @@ timeBlock.append(blockText);
 // 
 var currentDay = parseInt(moment().format('H'));
 
+
+
+// time blocks COLOR CODED to indicate past, present, or future
+$("textarea").each(function () {
+  var $this = $(this);
+  var id = parseInt($this.attr("id"));
+
+  if (id < currentHour) {
+    $(this).addClass("past");
+  }
+  if (id > currentHour) {
+    $(this).addClass("future");
+  }
+  if (id === currentHour) {
+    $(this).addClass("present");
+  }
+});
+
+
+$("button.saveBtn").click(function (event, loadEvents) {
+    event.preventDefault();
+  
+ 
+    var $element = $(this).siblings("textarea");
+    
+    var time = $element.attr("id");
+    console.log(time);
+   
+    $(".saveBtn").hover(function () {
+      $(this).addClass("saveBtn:hover");
+    });
+    // save events to localStorage
+    if (time && text !== "") {
+      console.log(time, text);
+      localStorage.setItem(time, text);
+    }
+  });
+  $(".saveBtn").hover(function () {
+    $(this).addClass("saveBtn:hover");
+  });
+  
+  fetchEvents();
